4a8e978bfc0becf83a0093a42eccb1c3a2824989
Dmitry <dmitry@smilecdr.com>
2025-04-13 21:26:14 -0600

    added component lazy loading

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -19,8 +19,7 @@ function City() {
 
   useEffect(() => {
     getCity(id);
-    // console.log("currentCity ", currentCity);
-  }, [id]);
+  }, [id, getCity]);
 
   const { cityName, emoji, date, notes } = currentCity;
 
